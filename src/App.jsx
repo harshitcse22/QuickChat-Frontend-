754f464500d3c4accdfd888226e637c9894e2e93
Harshit <hktiwari980@gmail.com>
2025-11-17 15:07:14 +0530

    AuthContext created

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import Profilepage from './pages/Profilepage'
+import {Toaster} from 'react-hot-toast';
 
 const App = () => {
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] 
        bg-contain">
+        <Toaster/>
        <Routes>
          <Route path='/' element={<HomePage/>}/>
          <Route path='/login' element={<LoginPage/>}/>
